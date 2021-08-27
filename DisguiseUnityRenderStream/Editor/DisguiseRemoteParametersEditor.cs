7b2b5fb6b3246e2f87b3155a3c137821169c20a4
Mike Cobb <mike@highlyinteractive.com>
2021-08-27 15:58:32 +0100

    Fixed PEBCAK error

--- DisguiseUnityRenderStream/Editor/DisguiseRemoteParametersEditor.cs
+++ DisguiseUnityRenderStream/Editor/DisguiseRemoteParametersEditor.cs
@@ -1,47 +1,48 @@
-﻿using UnityEditor;
-using UnityEditorInternal;
-using Disguise.RenderStream;
+﻿    using UnityEngine;
+    using UnityEditor;
+    using UnityEditorInternal;
+    using Disguise.RenderStream;
 
-[CustomEditor(typeof(DisguiseRemoteParameters))]
-public class DisguiseRemoteParametersEditor : Editor
-{
-    private SerializedProperty _fieldsProp;
-
-    public override void OnInspectorGUI()
+    [CustomEditor(typeof(DisguiseRemoteParameters))]
+    public class DisguiseRemoteParametersEditor : Editor
     {
-        base.OnInspectorGUI();
+        private SerializedProperty _fieldsProp;
 
-        serializedObject.Update();
-
-        ReorderableListUtility.DoLayoutListWithFoldout(list);
-
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Select All"))
+        public override void OnInspectorGUI()
         {
-            for (int i = 0; i < _fieldsProp.arraySize; i++)
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            ReorderableListUtility.DoLayoutListWithFoldout(list);
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Select All"))
             {
-                var element = _fieldsProp.GetArrayElementAtIndex(i);
-                element.FindPropertyRelative("exposed").boolValue = true;
+                for (int i = 0; i < _fieldsProp.arraySize; i++)
+			    {
+                    var element = _fieldsProp.GetArrayElementAtIndex(i);
+                    element.FindPropertyRelative("exposed").boolValue = true;
+                }
             }
+            if (GUILayout.Button("Select None"))
+            {
+                for (int i = 0; i < _fieldsProp.arraySize; i++)
+                {
+                    var element = _fieldsProp.GetArrayElementAtIndex(i);
+                    element.FindPropertyRelative("exposed").boolValue = false;
+                }
+            }
+            GUILayout.EndHorizontal();
+
+            serializedObject.ApplyModifiedProperties();
         }
-        if (GUILayout.Button("Select None"))
+
+        private void OnEnable()
         {
-            for (int i = 0; i < _fieldsProp.arraySize; i++)
-            {
-                var element = _fieldsProp.GetArrayElementAtIndex(i);
-                element.FindPropertyRelative("exposed").boolValue = false;
-            }
+            _fieldsProp = this.serializedObject.FindProperty("fields");
+            list = ReorderableListUtility.CreateAutoLayout(_fieldsProp, true, true, false, false);
         }
-        GUILayout.EndHorizontal();
 
-        serializedObject.ApplyModifiedProperties();
+        private ReorderableList list;
     }
-
-    private void OnEnable()
-    {
-        _fieldsProp = this.serializedObject.FindProperty("fields", true, true, false, false);
-        list = ReorderableListUtility.CreateAutoLayout(property);
-    }
-
-    private ReorderableList list;
-}
