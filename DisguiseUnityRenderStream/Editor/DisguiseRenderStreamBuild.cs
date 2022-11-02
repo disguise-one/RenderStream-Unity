b69de2567cb2d7d5d31f0e0d48304290c9151ee8
Wen Zhang <wen.zhang@unity3d.com>
2022-11-02 09:45:27 -0400

    More robust scene post-processing

--- DisguiseUnityRenderStream/Editor/DisguiseRenderStreamBuild.cs
+++ DisguiseUnityRenderStream/Editor/DisguiseRenderStreamBuild.cs
@@ -1,102 +1,24 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace Disguise.RenderStream
 {
-    class DisguiseRenderStreamBuildProcessor  : UnityEditor.Build.IPreprocessBuildWithReport
+    class DisguiseRenderStreamBuildProcessor  : UnityEditor.Build.IPostprocessBuildWithReport
     {
-        static ManagedSchema schema;
-        public int callbackOrder
+        public int callbackOrder { get; set; } = 0;
+        
+        public void OnPostprocessBuild(BuildReport report)
         {
-            get { return 0; }
-        }
-
-        public void OnPreprocessBuild(UnityEditor.Build.Reporting.BuildReport report)
-        {
-            DisguiseRenderStreamSettings settings = DisguiseRenderStreamSettings.GetOrCreateSettings();
-            schema = new ManagedSchema();
-            schema.channels = new string[0];
-            switch (settings.sceneControl)
-            {
-                case DisguiseRenderStreamSettings.SceneControl.Selection:
-                    Debug.Log("Generating scene-selection schema for: " + SceneManager.sceneCountInBuildSettings + " scenes");
-                    schema.scenes = new ManagedRemoteParameters[SceneManager.sceneCountInBuildSettings];
-                    if (SceneManager.sceneCountInBuildSettings == 0)
-                        Debug.LogWarning("No scenes in build settings. Schema will be empty.");
-                    break;
-                case DisguiseRenderStreamSettings.SceneControl.Manual:
-                default:
-                    Debug.Log("Generating manual schema");
-                    schema.scenes = new ManagedRemoteParameters[1];
-                    break;
-            }
-        }
-
-        [UnityEditor.Callbacks.PostProcessSceneAttribute(0)]
-        static void OnPostProcessScene()
-        {
-            if (!UnityEditor.BuildPipeline.isBuildingPlayer)
-                return;
-
-            Scene activeScene = SceneManager.GetActiveScene();
-            DisguiseRenderStreamSettings settings = DisguiseRenderStreamSettings.GetOrCreateSettings();
-            switch (settings.sceneControl)
-            {
-                case DisguiseRenderStreamSettings.SceneControl.Selection:
-                {
-                    if (activeScene.buildIndex < 0 || activeScene.buildIndex >= SceneManager.sceneCountInBuildSettings)
-                    {
-                        Debug.Log("Ignoring scene: " + activeScene.name + " (not in build's scene list)");
-                        return;
-                    }
-
-                    Debug.Log("Processing scene: " + activeScene.name + " (" + activeScene.buildIndex + '/' + SceneManager.sceneCountInBuildSettings + ')');
-
-                    HashSet<string> channels = new HashSet<string>(schema.channels);
-                    channels.UnionWith(getTemplateCameras().Select(camera => camera.name));
-                    schema.channels = channels.ToArray();
-
-                    List<ManagedRemoteParameter> parameters = new List<ManagedRemoteParameter>();
-                    foreach (var parameter in UnityEngine.Object.FindObjectsOfType(typeof(DisguiseRemoteParameters)) as DisguiseRemoteParameters[])
-                        parameters.AddRange(parameter.exposedParameters());
-                    schema.scenes[activeScene.buildIndex] = new ManagedRemoteParameters();
-                    ManagedRemoteParameters scene = schema.scenes[activeScene.buildIndex];
-                    scene.name = activeScene.name;
-                    scene.parameters = parameters.ToArray();
-                    break;
-                }
-                case DisguiseRenderStreamSettings.SceneControl.Manual:
-                default:
-                {
-                    Debug.Log("Processing scene: " + activeScene.name);
-
-                    HashSet<string> channels = new HashSet<string>(schema.channels);
-                    channels.UnionWith(getTemplateCameras().Select(camera => camera.name));
-                    schema.channels = channels.ToArray();
-
-                    if (schema.scenes[0] == null)
-                    {
-                        schema.scenes[0] = new ManagedRemoteParameters();
-                        schema.scenes[0].parameters = new ManagedRemoteParameter[0];
-                    }
-
-                    ManagedRemoteParameters scene = schema.scenes[0];
-                    scene.name = "Default";
-                    List<ManagedRemoteParameter> parameters = new List<ManagedRemoteParameter>(scene.parameters);
-                    foreach (var parameter in UnityEngine.Object.FindObjectsOfType(typeof(DisguiseRemoteParameters)) as DisguiseRemoteParameters[])
-                        parameters.AddRange(parameter.exposedParameters());
-                    scene.parameters = parameters.ToArray();
-                    break;
-                }
-            }
-        }
-
-        [UnityEditor.Callbacks.PostProcessBuildAttribute(0)]
-        static void OnPostProcessBuild(UnityEditor.BuildTarget target, string pathToBuiltProject)
-        {
-            if (target != UnityEditor.BuildTarget.StandaloneWindows64)
+            var target = report.summary.platform;
+            
+            if (target != BuildTarget.StandaloneWindows64)
             {
                 Debug.LogError("DisguiseRenderStream: RenderStream is only available for 64-bit Windows (x86_64).");
                 return;
@@ -107,12 +29,85 @@ namespace Disguise.RenderStream
                 Debug.LogError("DisguiseRenderStream: RenderStream DLL not available, could not save schema");
                 return;
             }
+            
+            DisguiseRenderStreamSettings settings = DisguiseRenderStreamSettings.GetOrCreateSettings();
+            var schema = new ManagedSchema
+            {
+                channels = Array.Empty<string>()
+            };
 
+            var allScenesInBuild = EditorBuildSettings.scenes;
+            
+            switch (settings.sceneControl)
+            {
+                case DisguiseRenderStreamSettings.SceneControl.Selection:
+                    Debug.Log("Generating scene-selection schema for: " + allScenesInBuild.Length + " scenes");
+                    schema.scenes = new ManagedRemoteParameters[allScenesInBuild.Length];
+                    if (allScenesInBuild.Length == 0)
+                        Debug.LogWarning("No scenes in build settings. Schema will be empty.");
+                    break;
+                case DisguiseRenderStreamSettings.SceneControl.Manual:
+                default:
+                    Debug.Log("Generating manual schema");
+                    schema.scenes = new ManagedRemoteParameters[1];
+                    break;
+            }
+            
+            foreach (var buildScene in allScenesInBuild)
+            {
+                if (!buildScene.enabled)
+                {
+                    continue;
+                }
+
+                var scene = SceneManager.GetSceneByPath(buildScene.path);
+                if (!scene.IsValid() || !scene.isLoaded)
+                {
+                    scene = EditorSceneManager.OpenScene(buildScene.path, OpenSceneMode.Single);
+                }
+
+                if (!scene.IsValid() || !scene.isLoaded)
+                {
+                    continue;
+                }
+
+                // In "Manual" mode (Disguise does not control scene changes), all parameters are listed under a single "Default" scene.
+                // In "Selection" mode (Disguise controls scene changes), parameters are listed under their respective scenes. 
+                var (sceneIndex, managedName, indexMessage) = settings.sceneControl switch {
+                    DisguiseRenderStreamSettings.SceneControl.Manual => (0, "Default", string.Empty),
+                    DisguiseRenderStreamSettings.SceneControl.Selection => (scene.buildIndex, scene.name, $"({scene.buildIndex}/{allScenesInBuild.Length})"),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                
+                Debug.Log($"Processing scene {scene.name} {indexMessage}");
+                AddSceneToSchema(schema, sceneIndex, managedName);
+            }
+
+            var pathToBuiltProject = report.summary.outputPath;
             RS_ERROR error = PluginEntry.instance.saveSchema(pathToBuiltProject, ref schema);
             if (error != RS_ERROR.RS_ERROR_SUCCESS)
             {
                 Debug.LogError(string.Format("DisguiseRenderStream: Failed to save schema {0}", error));
             }
+        }
+        
+        static void AddSceneToSchema(ManagedSchema schema, int sceneIndex, string name)
+        {
+            var channels = new HashSet<string>(schema.channels);
+            channels.UnionWith(Camera.allCameras.Select(camera => camera.name));
+            schema.channels = channels.ToArray();
+            schema.scenes[sceneIndex] ??= new ManagedRemoteParameters
+            {
+                name = name,
+                parameters = Array.Empty<ManagedRemoteParameter>()
+            };
+            var currentScene = schema.scenes[sceneIndex];
+
+            var parameters = currentScene.parameters
+                .Concat(Object.FindObjectsOfType<DisguiseRemoteParameters>()
+                .SelectMany(p => p.exposedParameters()));
+            
+            currentScene.parameters = parameters.ToArray();
         }
     }
 }
