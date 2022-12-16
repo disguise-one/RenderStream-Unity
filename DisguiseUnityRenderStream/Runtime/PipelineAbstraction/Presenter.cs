b223802aba625dfbc10bf76de386eaa48ddaf724
Mark Beiline <mark.beiline@unity3d.com>
2022-12-16 16:58:46 -0500

    Presenter: handle Y flip and better strategy names

--- DisguiseUnityRenderStream/Runtime/PipelineAbstraction/Presenter.cs
+++ DisguiseUnityRenderStream/Runtime/PipelineAbstraction/Presenter.cs
@@ -10,11 +10,11 @@ namespace Disguise.RenderStream
         public enum Strategy
         {
             Fill,
-            CenterWithoutResizing,
+            NoResize,
             FitWidth,
             FitHeight,
-            Fit,
-            Crop
+            Letterbox,
+            FillAspectRatio
         }
 
         public static Vector4 DoStrategy(Strategy strategy, Vector2 srcSize, Vector2 dstSize)
@@ -23,16 +23,16 @@ namespace Disguise.RenderStream
             {
                 case Strategy.Fill:
                     return Fill(srcSize, dstSize);
-                case Strategy.CenterWithoutResizing:
-                    return CenterWithoutResizing(srcSize, dstSize);
+                case Strategy.NoResize:
+                    return NoResize(srcSize, dstSize);
                 case Strategy.FitWidth:
                     return FitWidth(srcSize, dstSize);
                 case Strategy.FitHeight:
                     return FitHeight(srcSize, dstSize);
-                case Strategy.Fit:
-                    return Fit(srcSize, dstSize);
-                case Strategy.Crop:
-                    return Crop(srcSize, dstSize);
+                case Strategy.Letterbox:
+                    return Letterbox(srcSize, dstSize);
+                case Strategy.FillAspectRatio:
+                    return FillAspectRatio(srcSize, dstSize);
                 default:
                     throw new NotImplementedException();
             }
@@ -43,7 +43,7 @@ namespace Disguise.RenderStream
             return new Vector4(1f, 1f, 0f, 0f);
         }
         
-        static Vector4 CenterWithoutResizing(Vector2 srcSize, Vector2 dstSize)
+        static Vector4 NoResize(Vector2 srcSize, Vector2 dstSize)
         {
             var scale = srcSize / dstSize;
             var offset = CenterUVOffset(scale);
@@ -67,7 +67,7 @@ namespace Disguise.RenderStream
             return new Vector4(xScale, 1f, xOffset, 0f);
         }
         
-        static Vector4 Fit(Vector2 srcSize, Vector2 dstSize)
+        static Vector4 Letterbox(Vector2 srcSize, Vector2 dstSize)
         {
             var scrAspect = AspectRatio(srcSize);
             var dstAspect = AspectRatio(dstSize);
@@ -78,7 +78,7 @@ namespace Disguise.RenderStream
                 return FitHeight(srcSize, dstSize);
         }
         
-        static Vector4 Crop(Vector2 srcSize, Vector2 dstSize)
+        static Vector4 FillAspectRatio(Vector2 srcSize, Vector2 dstSize)
         {
             var scrAspect = AspectRatio(srcSize);
             var dstAspect = AspectRatio(dstSize);
@@ -109,11 +109,13 @@ namespace Disguise.RenderStream
     {
         const string k_profilerTag = "Disguise Presenter";
 
-        public PresenterStrategy.Strategy m_strategy = PresenterStrategy.Strategy.Fit;
+        public PresenterStrategy.Strategy m_strategy = PresenterStrategy.Strategy.FillAspectRatio;
         public RenderTexture m_source;
         public RenderTexture m_target;
 
         bool IsValid => m_source != null;
+
+        bool targetIsScreen => m_target == null;
 
         Vector2 sourceSize => new Vector2(m_source.width, m_source.height);
         
@@ -121,7 +123,7 @@ namespace Disguise.RenderStream
         {
             get
             {
-                if (m_target == null)
+                if (targetIsScreen)
                     return new Vector2(Screen.width, Screen.height);
                 else
                     return new Vector2(m_target.width, m_target.height);
@@ -162,6 +164,13 @@ namespace Disguise.RenderStream
             
             var srcScaleBias = new Vector4(1f, 1f, 0f, 0f);
             var dstScaleBias = PresenterStrategy.DoStrategy(m_strategy, sourceSize, targetSize);
+
+            if (targetIsScreen)
+            {
+                dstScaleBias.y = -dstScaleBias.y;
+                dstScaleBias.w = 1f - dstScaleBias.w;
+            }
+            
             Blitter.BlitQuad(cmd, m_source, srcScaleBias, dstScaleBias, 0, true);
         }
     }
